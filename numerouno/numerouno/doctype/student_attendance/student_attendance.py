aaceb7eab0ea06ae55fc49f8ab4991c33ede36ea
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-04-16 18:21:36 +0500

    chore: student attendance function py added

--- numerouno/numerouno/doctype/student_attendance/student_attendance.py
+++ numerouno/numerouno/doctype/student_attendance/student_attendance.py
@@ -6,8 +6,8 @@ def attendance_restriction(doc, method):
     if not doc.custom_student_signature:
         frappe.throw("""
             <div style="text-align:center; padding: 10px;">
-                <img src="/assets/frappe/images/ui-alert.svg" width="60" style="margin-bottom: 10px;" />
-                <h3 style="color:#d9534f;">Signature Required</h3>
-                <p style="font-size:14px;">Please provide your signature in the <b>Signature</b> field before submitting the attendance.</p>
-            </div>
+                        <img src="https://cdn-icons-png.flaticon.com/512/463/463612.png" width="60" style="margin-bottom: 10px;" />
+                        <h3 style="color:#d9534f;">Signature Missing</h3>
+                        <p style="font-size:14px;">Please provide your signature in the <b>Signature</b> field before submitting the attendance.</p>
+                    </div>
         """)
