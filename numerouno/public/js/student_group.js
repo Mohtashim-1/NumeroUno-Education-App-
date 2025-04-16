d3366c7d7e4f657015fcc51beb9cbeb4b50c9bce
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-04-16 17:15:00 +0500

    chore: student group feat added

---
+++ numerouno/public/js/student_group.js
@@ -0,0 +1,58 @@
+frappe.ui.form.on('Student Group', {
+    refresh: function(frm) {
+        frm.add_custom_button(__('Create Coarse Schedule'), () => {
+            show_coarse_dialog(frm);
+        }, __('Actions'));
+
+        
+    }
+});
+
+function show_coarse_dialog(frm) {
+    const dialog = new frappe.ui.Dialog({
+        title: __('Create Student Schedule'),
+        fields: [
+            {
+                label: __('From Time'),
+                fieldname: 'from_time',
+                fieldtype: 'Time',
+                reqd: 1,
+            },
+            {
+                label: __('To Time'),
+                fieldname: 'to_time',
+                fieldtype: 'Time',
+                reqd: 1,
+            }
+        ],
+        primary_action_label: __('Create'),
+        primary_action(values) {
+            if (!values) return;
+
+            frappe.call({
+                method: "numerouno.numerouno.doctype.student_group.student_group.create_coarse_schedule",
+                args: {
+                    student_group: frm.doc.name,
+                    from_time: values.from_time,
+                    to_time: values.to_time
+                },
+                callback: function(r) {
+                    if (!r.exc) {
+                        frappe.show_alert({
+                            message: __('Coarse Schedule Created'),
+                            indicator: 'green'
+                        });
+                    }
+                },
+                freeze: true,
+                freeze_message: __('Creating Coarse Schedule...')
+            });
+
+            dialog.hide();
+        }
+    });
+
+    dialog.show();
+}
+
+
